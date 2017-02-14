a97a1276bd8770d7b1e4139f0ff1a565ad2261c6
info-infoman <info_infoman@bk.ru>
2017-02-14 23:47:06 +0400

    Update Brushless.ino

--- Brushless.ino
+++ Brushless.ino
@@ -5,19 +5,27 @@ unsigned int n = 0, timer2_initial_value, s = 0;
 void setup(){
   InitTimersSafe();
   bool success = SetPinFrequencySafe(9, frequency);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
+  //phase
+  pinMode(7, OUTPUT);//3
   pinMode(8, OUTPUT);
-  pinMode(13, OUTPUT);
-  pinMode(0, INPUT);
-  pinMode(1, INPUT);
+  
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);//8
+  //
+  pinMode(13, OUTPUT);//led
+  
+  //hall
   pinMode(2, INPUT);
-  pinMode(10, INPUT_PULLUP);
-  pinMode(11, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  
+  
+  pinMode(4, INPUT);//dir
+  
+  //pinMode(12, INPUT_PULLUP);
   // initialize timer2 interrupt for adc reading 
   noInterrupts();           // disable all interrupts
   TCCR2A = 0;
@@ -34,80 +42,80 @@ void setup(){
   n++;
   if (n>20){
     n = 0;
-   if (s != analogRead(A0)){
-   s = analogRead(A0); 
+   if (s != analogRead(A7)){
+   s = analogRead(A7); 
   pwmWrite(9,s/4);}}
  }
 int fwd(){
    while(1){
-if (digitalRead(2)==1){
-    if (digitalRead(1)==0){
-      if (digitalRead(0)==1){
-        digitalWrite(8,0);digitalWrite(7,0);digitalWrite(6,0);
-        digitalWrite(5,1);digitalWrite(4,1);digitalWrite(3,0);}
+if (digitalRead(4)==1){
+    if (digitalRead(3)==0){
+      if (digitalRead(2)==1){
+        digitalWrite(12,0);digitalWrite(11,0);digitalWrite(10,0);
+        digitalWrite(9,1);digitalWrite(8,1);digitalWrite(7,0);}
       else {
-        digitalWrite(8,1);digitalWrite(7,0);digitalWrite(6,0);
-        digitalWrite(5,1);digitalWrite(4,0);digitalWrite(3,0);}}
+        digitalWrite(12,1);digitalWrite(11,0);digitalWrite(10,0);
+        digitalWrite(9,1);digitalWrite(8,0);digitalWrite(7,0);}}
         
-     if (digitalRead(1)==1){
-      if (digitalRead(0)==0){
-        digitalWrite(8,1);digitalWrite(7,0);digitalWrite(6,0);
-        digitalWrite(5,0);digitalWrite(4,0);digitalWrite(3,1);}}}
+     if (digitalRead(3)==1){
+      if (digitalRead(2)==0){
+        digitalWrite(12,1);digitalWrite(11,0);digitalWrite(10,0);
+        digitalWrite(9,0);digitalWrite(8,0);digitalWrite(7,1);}}}
         
    
- if (digitalRead(2)==0){
-    if (digitalRead(1)==1){
-      if (digitalRead(0)==0){
-        digitalWrite(8,0);digitalWrite(7,0);digitalWrite(6,1);
-        digitalWrite(5,0);digitalWrite(4,0);digitalWrite(3,1);}
+ if (digitalRead(4)==0){
+    if (digitalRead(3)==1){
+      if (digitalRead(2)==0){
+        digitalWrite(12,0);digitalWrite(11,0);digitalWrite(10,1);
+        digitalWrite(9,0);digitalWrite(8,0);digitalWrite(7,1);}
       else {
-        digitalWrite(8,0);digitalWrite(7,1);digitalWrite(6,1);
-        digitalWrite(5,0);digitalWrite(4,0);digitalWrite(3,0);}}
+        digitalWrite(12,0);digitalWrite(11,1);digitalWrite(10,1);
+        digitalWrite(9,0);digitalWrite(8,0);digitalWrite(7,0);}}
         
-     if (digitalRead(1)==0){
-      if (digitalRead(0)==1){
-        digitalWrite(8,0);digitalWrite(7,1);digitalWrite(6,0);
-        digitalWrite(5,0);digitalWrite(4,1);digitalWrite(3,0);}}}
-   if (digitalRead(12)==0) break;}
+     if (digitalRead(3)==0){
+      if (digitalRead(2)==1){
+        digitalWrite(12,0);digitalWrite(11,1);digitalWrite(10,0);
+        digitalWrite(9,0);digitalWrite(8,1);digitalWrite(7,0);}}}
+   if (digitalRead(5)==0) break;}
 }
  int bwd(){
    while(1){
-if (digitalRead(2)==1){
-    if (digitalRead(1)==0){
-      if (digitalRead(0)==1){
-        digitalWrite(8,0);digitalWrite(7,0);digitalWrite(6,1);
-        digitalWrite(5,0);digitalWrite(4,0);digitalWrite(3,1);}
+if (digitalRead(4)==1){
+    if (digitalRead(3)==0){
+      if (digitalRead(2)==1){
+        digitalWrite(12,0);digitalWrite(11,0);digitalWrite(10,1);
+        digitalWrite(9,0);digitalWrite(8,0);digitalWrite(7,1);}
       else {
-        digitalWrite(8,0);digitalWrite(7,1);digitalWrite(6,1);
-        digitalWrite(5,0);digitalWrite(4,0);digitalWrite(3,0);}}
+        digitalWrite(12,0);digitalWrite(11,1);digitalWrite(10,1);
+        digitalWrite(9,0);digitalWrite(8,0);digitalWrite(7,0);}}
         
-     if (digitalRead(1)==1){
-      if (digitalRead(0)==0){
-        digitalWrite(8,0);digitalWrite(7,1);digitalWrite(6,0);
-        digitalWrite(5,0);digitalWrite(4,1);digitalWrite(3,0);}}}
+     if (digitalRead(3)==1){
+      if (digitalRead(2)==0){
+        digitalWrite(12,0);digitalWrite(11,1);digitalWrite(10,0);
+        digitalWrite(9,0);digitalWrite(8,1);digitalWrite(7,0);}}}
         
    
- if (digitalRead(2)==0){
-    if (digitalRead(1)==1){
-      if (digitalRead(0)==0){
-        digitalWrite(8,0);digitalWrite(7,0);digitalWrite(6,0);
-        digitalWrite(5,1);digitalWrite(4,1);digitalWrite(3,0);}
+ if (digitalRead(4)==0){
+    if (digitalRead(3)==1){
+      if (digitalRead(2)==0){
+        digitalWrite(12,0);digitalWrite(11,0);digitalWrite(10,0);
+        digitalWrite(9,1);digitalWrite(8,1);digitalWrite(7,0);}
       else {
-        digitalWrite(8,1);digitalWrite(7,0);digitalWrite(6,0);
-        digitalWrite(5,1);digitalWrite(4,0);digitalWrite(3,0);}}
+        digitalWrite(12,1);digitalWrite(11,0);digitalWrite(10,0);
+        digitalWrite(9,1);digitalWrite(8,0);digitalWrite(7,0);}}
         
-     if (digitalRead(1)==0){
-      if (digitalRead(0)==1){
-        digitalWrite(8,1);digitalWrite(7,0);digitalWrite(6,0);
-        digitalWrite(5,0);digitalWrite(4,0);digitalWrite(3,1);}}}
-   if (digitalRead(12)==0) break;}
+     if (digitalRead(3)==0){
+      if (digitalRead(2)==1){
+        digitalWrite(12,1);digitalWrite(11,0);digitalWrite(10,0);
+        digitalWrite(9,0);digitalWrite(8,0);digitalWrite(7,1);}}}
+   if (digitalRead(5)==0) break;}
 }
      
 void loop(){
   digitalWrite(13, LOW);
   pwmWrite(9,s);
-  digitalWrite(8,0);digitalWrite(7,0);digitalWrite(6,0);
-  digitalWrite(5,0);digitalWrite(4,0);digitalWrite(3,0);
-  if (digitalRead(10)==0) {digitalWrite(13, HIGH);bwd();}
-  if (digitalRead(11)==0) {digitalWrite(13, HIGH);fwd();}
+  digitalWrite(12,0);digitalWrite(11,0);digitalWrite(10,0);
+  digitalWrite(9,0);digitalWrite(8,0);digitalWrite(7,0);
+  if (digitalRead(4)==0) {digitalWrite(13, HIGH);bwd();}
+  if (digitalRead(4)==1) {digitalWrite(13, HIGH);fwd();}
 }
